115036e06af1a2bb1967b9bb1eba08a82eaa9221
wx <wangxiang_hfut@outlook.com>
2026-01-03 23:21:05 +0800

    A825 CAN interface

---
+++ ARINC_Canif.c
@@ -0,0 +1,323 @@
+#include <canlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+#include "ARINC_Canif.h"
+
+
+typedef enum
+{
+    CHANNEL_CLOSE,
+    CHANNEL_OPEN,
+    CHANNEL_BUSY,
+    CHANNEL_IDLE
+} CanChannelStatus_Type;
+
+typedef struct
+{
+    int channel_handle;
+    int channel_status;
+} Can_channel_type;
+
+static void check(char *id, canStatus stat)
+{
+    if (stat != canOK) {
+        char buf[50];
+        buf[0] = '\0';
+        canGetErrorText(stat, buf, sizeof(buf));
+        printf("%s: failed, stat=%d (%s)\n", id, (int)stat, buf);
+    }
+}
+
+static int get_channel_num (int *chanCount)
+{
+    canStatus stat;
+    stat = canGetNumberOfChannels(chanCount);
+    return stat;
+}
+
+int Open_channel_fun(int ChId, ChannelInfo *info, int *hnd_ptr)
+{
+    canHandle hnd;
+    canStatus stat;
+    int chanCount;
+
+    *hnd_ptr = -1;
+
+    canInitializeLibrary();
+
+    if (canOK != get_channel_num(&chanCount))
+    {
+        perror("Get can channel info error\n");
+        return canERR_NOTFOUND;
+    }
+
+    if (info == NULL || ChId >= chanCount)
+    {
+        return A825_ERR_INVALIDARG;
+    }
+
+    if (info->type == A825_CANFD)
+    {
+        printf("Open a can channel as CANFD\n");
+
+        hnd = canOpenChannel(ChId, canOPEN_CAN_FD | canOPEN_REQUIRE_EXTENDED);
+        *hnd_ptr = hnd;
+        if (hnd < 0) {
+            printf("canOpenChannel %d\n", ChId);
+            check("", hnd);
+            return A825_CHANNEL_UNAVAILABLE;
+        }
+
+        if (info->prop[0].bitrate != 0 && info->prop[1].bitrate != 0)
+        {
+            if ((info->prop[0].phase1 == 0 && info->prop[0].phase2 == 0) \
+                || (info->prop[1].phase1 == 0 && info->prop[1].phase2 == 0))
+            {
+                stat = canSetBusParams(hnd, info->prop[0].bitrate, 0, 0, 0, 0, 0);
+                check("canSetBusParams", stat);
+                if (stat != canOK) {
+                    Arinc_Canif.Close_channel(hnd);
+                    return A825_CHANNEL_UNAVAILABLE;
+                }
+                stat = canSetBusParamsFd(hnd, info->prop[1].bitrate, 0, 0, 0);
+                check("canSetBusParamsFd", stat);
+                if (stat != canOK) {
+                    Arinc_Canif.Close_channel(hnd);
+                    return A825_CHANNEL_UNAVAILABLE;
+                }
+            }
+            else
+            {
+                stat = canSetBusParams(hnd, info->prop[0].bitrate, info->prop[0].phase1 + info->prop[0].prop, info->prop[0].phase2, info->prop[0].sjw, 1, 0);
+                check("canSetBusParams", stat);
+                if (stat != canOK) {
+                    Arinc_Canif.Close_channel(hnd);
+                    return A825_CHANNEL_UNAVAILABLE;
+                }
+                stat = canSetBusParamsFd(hnd, info->prop[1].bitrate, info->prop[1].phase1 + info->prop[1].prop, info->prop[1].phase2, info->prop[1].sjw);
+                check("canSetBusParamsFd", stat);
+                if (stat != canOK) {
+                    Arinc_Canif.Close_channel(hnd);
+                    return A825_CHANNEL_UNAVAILABLE;
+                }
+            }
+            stat = canSetBusOutputControl(hnd, info->driver_mode);
+            check("canSetBusOutputControl", stat);
+            if (stat != canOK) {
+                Arinc_Canif.Close_channel(hnd);
+                return A825_CHANNEL_UNAVAILABLE;
+            }
+            stat = canBusOn(hnd);
+            check("canBusOn", stat);
+            if (stat != canOK) {
+                Arinc_Canif.Close_channel(hnd);
+                return A825_CHANNEL_UNAVAILABLE;
+            }  
+        } 
+        else
+        {
+            printf("info error\n");
+            return A825_ERR_INVALIDARG;
+        }
+    }
+    else if (info->type == A825_CAN)
+    {
+        printf("Open a can channel as can\n");
+        hnd = canOpenChannel(ChId, canOPEN_REQUIRE_EXTENDED);
+        *hnd_ptr = hnd;
+        if (hnd < 0) {
+            printf("canOpenChannel %d", ChId);
+            check("", hnd);
+            return A825_CHANNEL_UNAVAILABLE;
+        }
+
+        if (info->prop[0].bitrate != 0)
+        {
+            if (info->prop[0].phase1 == 0 && info->prop[0].phase2 == 0)
+            {
+                stat = canSetBusParams(hnd, info->prop[0].bitrate, 0, 0, 0, 0, 0);
+                check("canSetBusParams", stat);
+                if (stat != canOK) {
+                    Arinc_Canif.Close_channel(hnd);
+                    return A825_CHANNEL_UNAVAILABLE;
+                }
+            }
+            else
+            {
+                stat = canSetBusParams(hnd, info->prop[0].bitrate, info->prop[0].phase1 + info->prop[0].prop, info->prop[0].phase2, info->prop[0].sjw, 1, 0);
+                check("canSetBusParams", stat);
+                if (stat != canOK) {
+                    Arinc_Canif.Close_channel(hnd);
+                    return A825_CHANNEL_UNAVAILABLE;
+                }
+            }
+
+            stat = canSetBusOutputControl(hnd, info->driver_mode);
+            check("canSetBusOutputControl", stat);
+            if (stat != canOK) {
+                Arinc_Canif.Close_channel(hnd);
+                return A825_CHANNEL_UNAVAILABLE;
+            }
+            stat = canBusOn(hnd);
+            check("canBusOn", stat);
+            if (stat != canOK) {
+                Arinc_Canif.Close_channel(hnd);
+                return A825_CHANNEL_UNAVAILABLE;
+            }  
+        } 
+        else
+        {
+            printf("info error\n");
+            return A825_ERR_INVALIDARG;
+        }
+    }
+    else
+    {
+        return A825_ERR_INVALIDARG;
+    }
+
+    return A825_SUCCESS;
+}
+
+int Close_channel_fun (int hnd)
+{
+    canStatus stat;
+    stat = canClose(hnd);
+    check("canClose", stat);
+    if (stat != canOK)
+    {
+        return A825_CHANNEL_UNAVAILABLE;
+    }
+    return A825_SUCCESS;
+}
+
+int send_frame_fun (int mode, int hnd, long messageid, unsigned char *data, unsigned int dlc, unsigned long timeout)
+{
+    canStatus stat;
+    int cap;
+    if (data == NULL)
+    {
+        printf("Import data invalid\n");
+        return A825_ERR_INVALIDARG;
+    }
+
+    stat = canGetHandleData(hnd, canCHANNELDATA_CHANNEL_FLAGS, &cap, sizeof(cap));
+    check("canGetHandleData", stat);
+    if (stat != canOK)
+    {
+        Arinc_Canif.Close_channel(hnd);
+        return A825_CHANNEL_UNAVAILABLE;
+    }
+    printf("Channel cap is 0x%x\n", cap);
+
+    if (cap & canCHANNEL_IS_CANFD)
+    {
+        printf("Send CanFd message\n");
+        if (mode == A825_SEND_SYNC)
+        {
+            stat = canWriteWait(hnd, messageid, data, dlc, canMSG_EXT | canFDMSG_FDF | canFDMSG_BRS, timeout);
+            check("canWriteWait FD", stat);
+            if (stat != canOK)
+            {
+                return A825_ERR_TIMEOUT;
+            }
+        }
+        else
+        {
+            stat = canWrite(hnd, messageid, data, dlc, canMSG_EXT | canFDMSG_FDF | canFDMSG_BRS);
+            check("canWrite FD", stat);
+            if (stat != canOK)
+            {
+                return A825_RESOURCE_UNAVAILABLE;
+            }
+        }
+    }
+    else if (cap & canCHANNEL_IS_OPEN)
+    {
+        printf("Send Can message\n");
+        if (dlc > 8)
+        {
+            return A825_ERR_INVALIDARG;
+        }
+        if (mode == A825_SEND_SYNC)
+        {
+            stat = canWriteWait(hnd, messageid, data, dlc, canMSG_EXT, timeout);
+            check("canWriteWait", stat);
+            if (stat != canOK)
+            {
+                return A825_ERR_TIMEOUT;
+            }               
+        }
+        else
+        {
+            stat = canWrite(hnd, messageid, data, dlc, canMSG_EXT);
+            check("canWrite", stat);
+            if (stat != canOK)
+            {
+                return A825_RESOURCE_UNAVAILABLE;
+            }
+        }           
+    }
+
+    return A825_SUCCESS;
+}
+
+
+int recv_frame_fun (int mode, int hnd, long *messageid, unsigned char *data, unsigned int *dlc, unsigned long *timestamp, unsigned long timesout)
+{
+    canStatus stat;
+    unsigned int canFlags;
+    if (data == NULL || messageid == NULL || dlc == NULL || timestamp == NULL)
+    {
+        printf("Import data invalid\n");
+        return A825_ERR_INVALIDARG;        
+    }
+
+    if (A825_RECEIVE_NONBLOCK == mode)
+    {
+        stat = canRead(hnd, messageid, data, dlc, &canFlags, timestamp);
+        check("canRead", stat);
+        if (stat != canOK) {
+            return A825_NO_DATA;
+        }        
+    }
+    else if (A825_RECEIVE_BLOCK == mode)
+    {
+        stat = canReadWait(hnd, messageid, data, dlc, &canFlags, timestamp, timesout);
+        check("canReadWait", stat);
+        if (stat != canOK) {
+            return A825_ERR_TIMEOUT;
+        }   
+    }
+    else
+    {
+        return A825_ERR_INVALIDARG;
+    }
+    return A825_SUCCESS;
+}
+
+
+int Check_Inbuf_Available_fun (int hnd, int timeout)
+{
+    canStatus stat;
+
+    stat = canWriteSync(hnd, timeout);
+    check("canWriteSync", stat);
+    if (stat != canOK) {
+        return A825_ERR_TIMEOUT;
+    }
+
+    return A825_SUCCESS;
+}
+
+
+const Arinc_Canif_Type Arinc_Canif =
+{
+    &Open_channel_fun,
+    &Close_channel_fun,
+    &send_frame_fun,
+    &recv_frame_fun,
+    &Check_Inbuf_Available_fun
+};
---
+++ ARINC_Canif.h
@@ -0,0 +1,199 @@
+#ifndef ARINC_CANIF_H
+#define ARINC_CANIF_H
+
+#define A825_SUCCESS                  0
+#define A825_NO_DATA                 -1
+#define A825_ERR_TIMEOUT             -2
+#define A825_ERR_INVALIDARG          -3
+#define A825_CHANNEL_UNAVAILABLE     -4
+#define A825_RESOURCE_UNAVAILABLE    -5
+#define A825_UNKNOWN_ERROR           -6
+
+
+enum a825_CanType
+{
+    A825_CAN = 0,
+    A825_CANFD = 1
+};
+
+enum A825_SendMode
+{
+    A825_SEND_ASYNC = 0,
+    A825_SEND_SYNC = 1
+};
+
+enum A825_ReceiveMode
+{
+    A825_RECEIVE_NONBLOCK = 0,
+    A825_RECEIVE_BLOCK = 1
+};
+
+
+typedef struct
+{
+    int type; //0: class can; 1: can fd
+    struct
+    {
+        int bitrate;
+        int tq;
+        int phase1;
+        int phase2;
+        int sjw;
+        int prop;
+        int prescaler;
+    } prop[2];
+    int driver_mode; //0: normal mode; 1: monitor mode
+} ChannelInfo;
+
+/**
+ *
+ * @brief Opens a CAN channel (circuit) and returns a handle which is used
+ * in subsequent calls to send, receive or close functions.
+ *
+ *
+ * Channel numbering is dependent on the installed hardware. The first channel
+ * always has number 0.
+ *
+ * For example,
+ *
+ * If you have a single LAPcan, the channels are numbered 0 and 1.
+ *
+ * If you have a USBcan Professional, the channels are numbered 0-1
+ *     according to the labels on the cables.
+ *
+ * The virtual channels come after all physical channels.
+ *
+ * If you are using multiple threads, note that the returned handle is usable
+ * only in the context of the thread that created it. That is, you must call
+ * Open_channel() in each of the threads in your application that uses the
+ * CAN bus. You can open the same channel from multiple threads, but you must
+ * call Open_channel() once per thread.
+ *
+ * If you are using the same channel via multiple handles, note that the
+ * default behaviour is that the different handles will "hear" each other just as
+ * if each handle referred to a channel of its own. If you open, say, channel 0
+ * from thread A and thread B and then send a message from thread A, it will be
+ * "received" by thread B.
+ *
+ * @note The handle returned may be zero which is perfectly valid.
+ *
+ * @param[in]  ChId  The number of the channel. Channel numbering is hardware dependent.
+ * @param[in]  info  Pointer to a ChannelInfo structure containing the configuration details for 
+ *                   the can channel
+ * @param[out] hnd_ptr  a handle to the opened circuit
+ * @return Return 0 on success, or a negative error code on failure.
+ *
+ */
+typedef int (* Open_channel_ptr) (int ChId, ChannelInfo *info, int *hnd_ptr);
+
+/**
+ * @brief Closes the channel associated with the handle. If no other threads
+ * are using the CAN circuit, it is taken off bus. The handle can not be
+ * used for further references to the channel, so any variable containing
+ * it should be zeroed.
+ *
+ *
+ * @param[in]  hnd  An open handle to a CAN channel.
+ *
+ * @return Return 0 on success, or a negative error code on failure.
+ */
+typedef int (* Close_channel_ptr) (int hnd);
+
+/**
+ *
+ *
+ * @brief This function sends a CAN or CANFD message. If the Sync mode is used, it will be returns when the message has been
+ * successfully transmitted, or the timeout expires. If the Async mode is used, the call returns immediately after queuing
+ * the message to the driver so the message has not necessarily been transmitted.
+ *
+ * If you are using the same channel via multiple handles, note that the
+ * default behaviour is that the different handles will "hear" each other just as
+ * if each handle referred to a channel of its own. If you open, say, channel 0
+ * from thread A and thread B and then send a message from thread A, it will be
+ * "received" by thread B.
+ *
+ * @param[in]  mode         The send mode (0 --- Async mode; 1 --- Sync mode) If the Async mode is used
+ *                          need to confirm that the data in the buffer has been sent completely by call
+ *                          the function of Check_Inbuf_Available().
+ * @param[in]  hnd          A handle to an open CAN circuit.
+ * @param[in]  messageid    The identifier of the CAN message to send.
+ * @param[in]  data         A pointer to the message data, or \c NULL.
+ * @param[in]  dlc          The length of the message in bytes.<br>
+ *                          For Classic CAN dlc can be at most 8, unless \ref
+ *                          canOPEN_ACCEPT_LARGE_DLC is used.<br> For CAN FD dlc
+ *                          can be one of the following 0-8, 12, 16, 20, 24, 32,
+ *                          48, 64.
+ * @param[in] timeout    The timeout, in milliseconds. 0xFFFFFFFF gives an
+ *                       infinite timeout.
+ * @return Return 0 on success, or a negative error code on failure.
+ */
+typedef int (* send_frame_ptr) (int mode, int hnd, long messageid, unsigned char *data, unsigned int dlc, unsigned long timeout);
+
+/**
+ *
+ * @brief Reads a message from the receive buffer. For the BLOCK mode, if no message is available, the
+ * function waits until a message arrives or a timeout occurs. However, for the NONBLOCK mode, if no 
+ * message is available, the function returns immediately with return code.
+ *
+ * If you are using the same channel via multiple handles, note that the
+ * default behaviour is that the different handles will "hear" each other just as
+ * if each handle referred to a channel of its own. If you open, say, channel 0
+ * from thread A and thread B and then send a message from thread A, it will be
+ * "received" by thread B.
+ *
+ * @param[in]   mode            The receive mode (0 --- NONBLOCK mode; 1 --- BLOCK mode).
+ * @param[in]   hnd             A handle to an open circuit.
+ * @param[out]  messageid       Pointer to a buffer which receives the CAN identifier.
+ *                              This buffer will only get the identifier. To determine
+ *                              whether this identifier was standard (11-bit) or extended
+ *                              (29-bit), and/or whether it was remote or not, or if it
+ *                              was an error frame, examine the contents of the flag
+ *                              argument.
+ * @param[out]  data            Pointer to the buffer which receives the message data.
+ *                              This buffer must be large enough (i.e. 8 bytes for
+ *                              classic CAN and up to 64 bytes for CAN FD).
+ * @param[out]  dlc             Pointer to a buffer which receives the message length.
+ * @param[out] timestamp        Pointer to a buffer which receives the message time stamp.
+ *
+ * @param[in]  timeout          If no message is immediately available, this parameter
+ *                              gives the number of milliseconds to wait for a message
+ *                              before returning. 0xFFFFFFFF gives an infinite timeout.
+ * @return Return 0 on success, or a negative error code on failure.
+ */
+typedef int (* recv_frame_ptr) (int mode, int hnd, long *messageid, unsigned char *data, unsigned int *dlc, unsigned long *timestamp, unsigned long timesout);
+
+/**
+ *
+ * @brief Waits until all CAN messages for the specified handle are sent, or the
+ * timeout period expires.
+ *
+ * @param[in]  hnd       A handle to an open CAN circuit.
+ * @param[in]  timeout   The timeout in milliseconds. 0xFFFFFFFF gives an
+ *                       infinite timeout.
+ * @return Return 0 on success, or a negative error code on failure.
+ */
+typedef int (* Check_Inbuf_Available_ptr) (int hnd, int timeout);
+
+/**
+ *
+ * @brief The function interfaces structure Type.
+ *
+ */
+typedef struct
+{
+    Open_channel_ptr Open_channel;
+    Close_channel_ptr Close_channel;
+    send_frame_ptr send_frame;
+    recv_frame_ptr recv_frame;
+    Check_Inbuf_Available_ptr Check_Inbuf_Available;
+} Arinc_Canif_Type;
+
+
+
+/**
+ *
+ * @brief The function interfaces structure dedlaration.
+ *
+ */
+extern const Arinc_Canif_Type Arinc_Canif;
+#endif
